b4f43af8212a72e0d884f5093f461af196d9b785
Santiago Pacini <santiagopacini@gmail.com>
2024-07-13 18:03:46 -0300

    sender email using hono and brevo

--- api/index.js
+++ api/index.js
@@ -1,11 +1,54 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from 'hono';
+import * as brevo from '@getbrevo/brevo';
+import { handle } from 'hono/vercel';
+
+const Brevo = new brevo.TransactionalEmailsApi();
+Brevo.setApiKey(
+  brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.API_KEY ?? ''
+);
+
+const sendEmail = new brevo.SendSmtpEmail();
 
 const app = new Hono().basePath('/api')
 
-app.get('/', (c) => {
-  return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
-})
+app.post('/send', async c => {
+  try {
+    const body = await c.req.json();
+    sendEmail.subject = `Propuesta de ${body['senderName']}`;
+    sendEmail.sender = {
+      email: process.env.SENDER_EMAIL ?? '',
+      name: body['senderName'],
+    };
+    sendEmail.to = [{ email: process.env.TO_SEND_EMAIL ?? '' }];
+    sendEmail.htmlContent = `
+    <html>
+      <head>
+        <title>Email Template</title>
+      </head>
+      <body>
+        <h1>Enviado por ${body['senderName']} - ${body['senderEmail']}</h1>
+        <h3>Servicios que quiere: ${body['services'].join(', ')}</h3>
+        <h4>${body['message']}</h4>
+      </body>
+    </html>
+  `;
+    await Brevo.sendTransacEmail(sendEmail);
+
+    return c.json({ message: 'Email Sended' }, 200);
+  } catch (error) {
+    const err = error
+    return c.json(
+      {
+        error: {
+          status: err.response.statusCode,
+          message: err.response.body.message,
+        },
+      },
+      500
+    );
+  }
+});
 
 const handler = handle(app);
 
